8345fb300025c25225625443a49e1f41b206f55b
grebo87 <grebo87@gmail.com>
2015-10-13 12:00:52 -0430

    update 2

--- Movimientos/update.php
+++ Movimientos/update.php
@@ -1,10 +1,11 @@
 <?php
 //resives el serial del equipo
 $serial= $_GET['serial'];
-print "<script>alert('Serial ".$serial."');</script>";
+echo $serial;
+//print "<script>alert('Serial ".$serial."');</script>";
 
 //print "<script>alert('En construccion');</script>";
-  print('<meta http-equiv="refresh" content="0; URL=javascript:history.back()">');
+  //print('<meta http-equiv="refresh" content="0; URL=javascript:history.back()">');
 
 
 ?>
